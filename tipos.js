064001d660609887178f82e3906b2e1e9df06657
edw-perzd <legna246.dav@gmail.com>
2023-09-08 14:23:11 -0600

    01 Comenzando con JS

---
+++ tipos.js
@@ -0,0 +1,33 @@
+console.log("Hola desde Node")
+
+// Comentarios de una línea
+
+/*
+    Para comentarios de bloque
+*/
+
+// Diferentes tipos de datos
+/* 
+Number
+Strings
+Boolean
+Object
+undefined
+*/ 
+
+console.log(123)
+console.log(typeof(123))
+
+console.log(2.3)
+console.log(typeof(2.3))
+
+console.log(true)
+console.log(typeof(true))
+
+console.log("a")
+console.log(typeof("a"))
+
+console.log([])
+console.log(typeof([]))
+
+console.log(undefined)
